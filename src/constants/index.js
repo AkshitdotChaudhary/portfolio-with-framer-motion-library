b960ada7bd62c40fe1674d63b8155c6908ca345b
Akshit-Chaudhary <akshitchaudhary640@gmail.com>
2024-08-31 13:32:30 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Lorem ipsum odor amet, consectetuer adipiscing elit. Nostra viverra parturient ex leo ligula velit nam. Penatibus nostra mi aliquam mus nulla sapien. Scelerisque mi ornare orci diam consequat tellus inceptos feugiat porttitor. Placerat nascetur consequat eleifend, hendrerit maecenas id torquent molestie. Turpis orci ornare enim turpis adipiscing. Dapibus eleifend est integer pellentesque sagittis quis! Dictumst hendrerit habitasse gravida id placerat commodo. Ex ultricies ut suspendisse eleifend ultrices eleifend scelerisque. Ac fusce non faucibus dapibus libero velit. Mauris vehicula erat dictum aptent risus. Mattis eleifend neque luctus hendrerit neque curabitur.`;
+
+export const ABOUT_TEXT = `Lorem ipsum odor amet, consectetuer adipiscing elit. Ipsum magnis sociosqu diam netus maximus justo sem. Facilisi blandit conubia litora aenean faucibus taciti fermentum ligula. Auctor facilisi nunc aliquet luctus nisi dapibus mi. Libero aliquet massa amet nullam habitasse dolor accumsan. Inceptos eleifend faucibus nunc lobortis sodales non bibendum. Nam quis hendrerit blandit et velit ex nisl dolor. Venenatis feugiat dignissim non; tempus purus rutrum metus conubia. Vulputate erat commodo donec placerat senectus mauris litora eleifend.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Lorem ipsum odor amet.",
+    company: "Lorem.",
+    description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum lacinia suscipit mattis class, dis nullam nunc viverra. Ad hac pharetra venenatis dictum volutpat suscipit felis. Bibendum habitant sociosqu vivamus, himenaeos commodo enim semper?`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Lorem ipsum odor amet.",
+    company: "Lorem",
+    description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Auctor phasellus odio sapien ipsum faucibus aliquam a nisi phasellus. Aenean dignissim tristique accumsan sapien, pulvinar vehicula luctus. Fringilla pharetra lacus nisl porttitor maecenas euismod. Facilisis posuere consectetur ridiculus fames sociosqu dignissim potenti ultrices nascetur.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Lorem ipsum odor amet..",
+    company: "Lorem",
+    description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas lobortis auctor hac quam facilisi commodo et. Tempus ridiculus elementum ante dolor ante facilisi hendrerit curae. Per potenti mollis convallis; himenaeos elit integer ultrices?`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Lorem ipsum odor amet.",
+    company: "Lorem",
+    description: `Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat turpis sem id nisi nibh. Gravida dolor tempor eros a himenaeos tristique eleifend facilisi class. Condimentum aliquet sociosqu elit dolor phasellus, suscipit non tellus erat. Ultricies aliquam dis dui pretium sem neque ac tellus.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Lorem ipsum odor amet.",
+    image: project1,
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus erat sollicitudin sem sit et egestas phasellus in.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Lorem ipsum odor amet.",
+    image: project2,
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Pulvinar augue tortor, tempus lorem fermentum sociosqu netus. Class donec quam torquent ornare massa quisque venenatis.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Lorem ipsum odor amet.",
+    image: project3,
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis cras aptent eget euismod amet sollicitudin sociosqu himenaeos. Suspendisse cursus platea mi lacus primis commodo varius. Inceptos sodales fermentum efficitur ornare leo massa volutpat.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Lorem ipsum odor amet.",
+    image: project4,
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor posuere congue metus ultricies netus. Pharetra massa vivamus imperdiet potenti pharetra litora curae amet turpis.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Lorem ipsum odor amet, consectetuer adipiscing elit.",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
